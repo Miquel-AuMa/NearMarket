8e3a40c95dc94ebf1307ff55f12e6f3120a2b849
Oliver Sanz <oliversanz1991@gmail.com>
2020-04-10 20:53:18 +0200

    SearchBar

---
+++ front/src/components/Home/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-input
+        placeholder="Buscador"
+        v-model="searchText"
+        clearable
+        @keyup.native="prepareSearch">
+    </el-input>
+    <el-radio v-model="searchType" label="1" @change="search">Producto</el-radio>
+    <el-radio v-model="searchType" label="2" @change="search">Lugar</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data () {
+    return {
+      searchText: '',
+      searchType: '1',
+      searchTimeout: {}
+    }
+  },
+  methods: {
+    search: function () {
+      console.log('Buscador [' + this.searchType + ']: ' + this.searchText)
+    },
+    prepareSearch: function () {
+      window.clearTimeout(this.searchTimeout)
+      this.searchTimeout = window.setTimeout(this.search, 500)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
