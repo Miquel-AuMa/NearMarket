45f1489fccedd75608a489edba1c90e552710e37
Degs <davidcalvomeier@protonmail.com>
2020-04-10 20:28:25 +0200

    Create Card Shop container and Card Shop

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home></Home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
---
+++ front/src/components/CardsShopContainer.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="card-container">
+    <el-row :gutter="10" :justify="'center'">
+      <el-col :xs="24" :sm="12" :md="8" :lg="6" :xl="1" v-for="shop in shops" :key="shop.id" class="padding-10">
+        <CardShop :shop="shop" ></CardShop>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import CardShop from './CardShop'
+export default {
+  components: {
+    CardShop
+  },
+  data () {
+    return {
+      shops: [
+        {
+          id: 1,
+          name: 'Tienda Manolo',
+          address: 'Calle Falsa 123',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          category: { name: 'Category 1', id: 1 }
+        },
+        {
+          id: 2,
+          name: 'Tienda María',
+          address: 'Calle Falsa 456',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          category: { name: 'Category 2', id: 2 }
+        },
+        {
+          id: 3,
+          name: 'Tienda Manolo',
+          address: 'Calle Falsa 123',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          category: { name: 'Category 3', id: 3 }
+        },
+        {
+          id: 4,
+          name: 'Tienda María',
+          address: 'Calle Falsa 456',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          category: { name: 'Category 4', id: 4 }
+        },
+        {
+          id: 5,
+          name: 'Tienda Manolo',
+          address: 'Calle Falsa 123',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          category: { name: 'Category 5', id: 5 }
+        },
+        {
+          id: 6,
+          name: 'Tienda María',
+          address: 'Calle Falsa 456',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          category: { name: 'Category 6', id: 6 }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .card-container{
+    width: 80%;
+    margin: 0 auto;
+  }
+  .padding-10{
+    padding:10px;
+  }
+</style>
