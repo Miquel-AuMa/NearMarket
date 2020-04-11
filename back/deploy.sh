c6633ad89e071d86af95cf47f2d3bb6d6fa51351
ibon <ibon.konesa@gmail.com>
2020-04-11 19:33:33 +0200

    Rebuild image

--- back/deploy.sh
+++ back/deploy.sh
@@ -1,3 +1,5 @@
 git pull origin master
+docker-compose build php
+docker-compose up php --force-recreate -d
 docker-compose exec -T php composer install
 docker-compose exec -T php php artisan migrate
