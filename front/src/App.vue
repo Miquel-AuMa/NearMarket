ea3c915c9fe3ec47fd612c7963a8e72843fc98e1
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2020-04-11 04:27:16 -0300

    Layouts

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,27 +1,13 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>
-        <NavBar/>
-      </el-header>
-      <el-main>
-          <Home />
-      </el-main>
-    </el-container>
+    <router-view />
   </div>
-
 </template>
 
 <script>
-import Home from '@/views/Home.vue'
-import NavBar from '@/components/generic/NavBar.vue'
 import './assets/style.scss'
 
 export default {
-  name: 'app',
-  components: {
-    Home,
-    NavBar
-  }
+  name: 'app'
 }
 </script>
---
+++ front/src/layouts/LoginLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-container>
+    <el-header>
+      <nav-bar />
+    </el-header>
+
+    <el-main>
+      <router-view />
+        <!-- <home /> -->
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import NavBar from '@/components/generic/NavBar.vue'
+
+export default {
+  name: 'MainLayout',
+
+  components: { NavBar }
+}
