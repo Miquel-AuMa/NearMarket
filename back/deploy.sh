561caca90519e8f2363397d2e97ff8636c059d65
ibon <ibon.konesa@gmail.com>
2020-04-11 19:37:13 +0200

    Recreate builded image

--- back/deploy.sh
+++ back/deploy.sh
@@ -1,5 +1,5 @@
 git pull origin master
 docker-compose build php
-docker-compose up php --force-recreate -d
+docker-compose up  -d --force-recreate php
 docker-compose exec -T php composer install
 docker-compose exec -T php php artisan migrate
