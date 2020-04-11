1a15a0bdb6f6ccc691e72fc7a993c9dc69654c0f
Miguel Manzano García <mmanzano@gmail.com>
2020-04-11 13:14:44 +0200

    Shop Type Controller

--- back/app/ShopType.php
+++ back/app/ShopType.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShopType extends Model
 {
-    //
+    protected $casts = [
+        'delivery' => 'boolean',
+        'schedule' => 'array'
+    ];
+
+    public function shops()
+    {
+        return $this->hasMany(Shop::class);
+    }
+
+    public static function makeOne(array $data)
+    {
+        $shopType = new ShopType();
+        $shopType->name = $data['name'];
+        $shopType->description = $data['description'];
+
+        $shopType->save();
+
+        $shopType->load('shops');
+
+        return $shopType;
+    }
+
+    public function updateMe(array $data)
+    {
+        $this->name = $data['name'] ?? $this->name;
+        $this->description = $data['description'] ?? $this->description;
+        $this->save();
+
+        $this->load('shops');
+
+        return $this;
+    }
 }
